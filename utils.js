2cc36ced9a206949a4c93e383c14466dd93b66d6
Mohammad <84781340+OrdinaryDude2634@users.noreply.github.com>
2024-09-30 23:42:17 +0330

    Add a newline for better readability

--- utils.js
+++ utils.js
@@ -18,6 +18,7 @@ export async function getAccountAge(steamHex, timeout) {
     const match = (await response.text()).match(/was created\s*on ([\w\s,]+)/);
     return howLongSinceDateInMonths(match[1]);
 }
+
 export function sleep(milliseconds) {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
